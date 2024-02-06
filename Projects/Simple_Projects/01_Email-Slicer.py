1123dfbff9b9c1d48610e7bb3dde7bf88199aa00
SaifulIslamShanto <hello.saifulislamshanto@gmail.com>
2024-02-06 19:04:02 +0600

    Email Slicer Small Project

--- Projects/Simple_Projects/01_Email-Slicer.py
+++ Projects/Simple_Projects/01_Email-Slicer.py
@@ -1,7 +1,21 @@
-email = input("Enter your EMail: ")
+""" email = input("Enter your EMail: ")
 index = email.index("@")
 
 userName = email[:index]
 doMain = email[index+1:]
 
-print(f"Your UserName Is {userName} and doamin is {doMain}")
+print(f"Your UserName Is {userName}\ndoamin is {doMain}") """
+
+
+
+# ======================================
+
+# -> User input
+email = input("Enter Your email: ")
+
+indexOfEmail = email.index("@")
+
+userNameOfEmail = email[:indexOfEmail]
+doaminOfEmail = email[indexOfEmail+1:]
+
+print(f"Your Email Is : {userNameOfEmail}\nYour Domain Is : {doaminOfEmail}")
